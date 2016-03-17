b67db9af6952b47f6ce80ea12fe6233414bb7303
twilt4rville <timothy.b.wilt@gmail.com>
2016-03-17 17:32:39 -0600

    finished quick submit and register form

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,41 @@
-import { select, text } from './helpers/dom';
+//import { select, text } from './helpers/dom';
+//select('.default_content')::text(`It works &#92;o/ !`);
 
-select('.default_content')::text(`It works &#92;o/ !`);
+/* global $: true */
+
+class FormToggle {
+  constructor(container) {
+    this.$container = container;
+    this.$toggleContainer = this.$container.find('.checkbox-container');
+    this.$toggle = this.$container.find('.speedCheckout');
+    this.$form = this.$container.find('.speedCheckout-form');
+
+    this.tick = false;
+
+    this.$toggle.on('click', () => {
+      this.ticker();
+    });
+  }
+
+  ticker() {
+    this.tick ? this.closeForm() : this.openForm();
+  }
+
+  closeForm() {
+    this.$form.slideUp();
+    this.$toggleContainer.removeClass('wantForm');
+    this.tick = false;
+  }
+
+  openForm() {
+    this.$form.slideDown();
+    this.$toggleContainer.addClass('wantForm');
+    this.tick = true;
+  }
+}
+
+$(() => {
+
+  new FormToggle($('.speedCheckout-container'));
+
+});
